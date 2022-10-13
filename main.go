35967be305c23d2951de00490d17799eba223522
caldev <caldart123@gmail.com>
2022-10-13 08:54:48 -0300

    test check parser func

--- main.go
+++ main.go
@@ -21,4 +21,5 @@ func main() {
 	if err != nil {
 		fmt.Print("Error found")
 	}
+
 }
---
+++ util/parser_test.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"catching-pokemons/models"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParserPokemonSuccess(t *testing.T) {
+	c := require.New(t)
+
+	body, err := ioutil.ReadFile("samples/pokeapi_response.json")
+	c.NoError(err)
+
+	var response models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &response)
+	c.NoError(err)
+
+	parsedPokemon, err := ParsePokemon(response)
+	c.NoError(err)
+
+	body, err = ioutil.ReadFile("samples/api_response.json")
+	c.NoError(err)
+
+	var expectPokemon models.Pokemon
+
+	err = json.Unmarshal([]byte(body), &expectPokemon)
+	c.NoError(err)
+
+	c.Equal(expectPokemon, parsedPokemon)
+
+}
