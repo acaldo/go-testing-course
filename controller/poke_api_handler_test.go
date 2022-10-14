8f343616c05b39b9a881aaa3fbb9f8734c16a6a7
caldev <caldart123@gmail.com>
2022-10-14 07:38:02 -0300

    TestGetPokemonFromPokeApiSuccessWithMocks

--- controller/poke_api_handler_test.go
+++ controller/poke_api_handler_test.go
@@ -3,9 +3,11 @@ package controller
 import (
 	"catching-pokemons/models"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"testing"
 
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,6 +20,32 @@ func TestGetPokemonFromPokeApiSuccess(t *testing.T) {
 
 	body, err := ioutil.ReadFile("samples/poke_api_read.json")
 	c.NoError(err)
+
+	var expected models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &expected)
+	c.NoError(err)
+
+	c.Equal(expected, pokemon)
+
+}
+
+func TestGetPokemonFromPokeApiSuccessWithMocks(t *testing.T) {
+	c := require.New(t)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	body, err := ioutil.ReadFile("samples/poke_api_read.json")
+	c.NoError(err)
+
+	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", "1")
+
+	httpmock.RegisterResponder("GET", request, httpmock.NewStringResponder(200, string(body)))
+
+	pokemon, err := GetPokemonFromPokeApi("1")
+	c.NoError(err)
+	c.NotEmpty(pokemon)
 
 	var expected models.PokeApiPokemonResponse
 
