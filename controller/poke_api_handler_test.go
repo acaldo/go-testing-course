b49902d3a5af1a3c43040d9f4af8a95ac44f1433
caldev <caldart123@gmail.com>
2022-10-14 06:17:26 -0300

    test check call pokeapi

---
+++ controller/poke_api_handler_test.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"catching-pokemons/models"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPokemonFromPokeApiSuccess(t *testing.T) {
+	c := require.New(t)
+
+	pokemon, err := GetPokemonFromPokeApi("1")
+	c.NoError(err)
+	c.NotEmpty(pokemon)
+
+	body, err := ioutil.ReadFile("samples/poke_api_read.json")
+	c.NoError(err)
+
+	var expected models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &expected)
+	c.NoError(err)
+
+	c.Equal(expected, pokemon)
+
+}
